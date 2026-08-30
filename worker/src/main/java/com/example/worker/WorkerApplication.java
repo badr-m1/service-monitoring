3f9bb9b9f64441b709c8edf3ba409f9480fea65b
badr <badrbm540@gmail.com>
2026-08-30 23:55:10 +0300

    add app annotations

--- worker/src/main/java/com/example/worker/WorkerApplication.java
+++ worker/src/main/java/com/example/worker/WorkerApplication.java
@@ -2,8 +2,14 @@ package com.example.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example")
+@EnableJpaRepositories("com.example.servicemonitoring.repository")
+@EntityScan("com.example.servicemonitoring.entity")
+@EnableScheduling
 public class WorkerApplication {
 
 	public static void main(String[] args) {
