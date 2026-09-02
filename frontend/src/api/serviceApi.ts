f8aee56a50bc4272258c7aa21dba6d4a9c493cac
badr <badrbm540@gmail.com>
2026-09-02 22:52:03 +0300

    api client implementation

--- frontend/src/api/serviceApi.ts
+++ frontend/src/api/serviceApi.ts
@@ -1,27 +1,43 @@
 import type { ServiceSummary } from "../Types/serviceSummary";
 import type { ServiceHistory } from "../Types/serviceHistory";
 import type { ServiceRequest } from "../Types/serviceRequest";
-import type { Service } from "../Types/Service";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://localhost:8080/services";
 
-export async function createService(serviceRequest: ServiceRequest): Promise<Service> {
-    const response = await fetch(`${API_URL}/services`, {
+export async function createService(serviceRequest: ServiceRequest): Promise<ServiceSummary> {
+    const response = await fetch(`${API_URL}`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(serviceRequest),
     });
-        if (!response.ok) {
-        throw new Error("Failed to fetch service");
+
+    if (!response.ok) {
+        throw new Error("Failed to create service");
     }
 
     return response.json();
 }
 
-export async function getServices(): Promise<ServiceSummary[]> {
-    const response = await fetch(`${API_URL}/services`);
+export async function updateService(id: number, name: string): Promise<ServiceSummary> {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name:name}),
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to update services");
+    }
+
+    return response.json();
+}
+
+export async function getAllServices(): Promise<ServiceSummary[]> {
+    const response = await fetch(`${API_URL}`);
 
     if (!response.ok) {
         throw new Error("Failed to fetch services");
@@ -31,21 +47,34 @@ export async function getServices(): Promise<ServiceSummary[]> {
 }
 
 export async function getService(id: number): Promise<ServiceSummary> {
-    const response = await fetch(`${API_URL}/services/${id}`);
+    const response = await fetch(`${API_URL}/${id}`);
 
     if (!response.ok) {
-        throw new Error("Failed to fetch service");
+        throw new Error("Failed to fetch service summary");
     }
 
     return response.json();
 }
 
-export async function getServiceHistory(id: number): Promise<ServiceSummary> {
-    const response = await fetch(`${API_URL}/services/${id}/history`);
+export async function getServiceHistory(id: number, startTime: Date, endTime: Date): Promise<ServiceHistory> {
+    const startTimeStr = startTime.toISOString();
+    const endTimeStr = endTime.toISOString();
+
+    const response = await fetch(`${API_URL}/services/${id}/history?startTime=${startTimeStr}&?endTime=${endTimeStr}`);
 
     if (!response.ok) {
-        throw new Error("Failed to fetch service");
+        throw new Error("Failed to fetch service history");
     }
 
     return response.json();
+}
+
+export async function deleteService(id: number): Promise<void> {
+    const response = await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+    });
+
+    if (!response.ok) {
+        throw new Error("Failed to delete service");
+    }
 }
